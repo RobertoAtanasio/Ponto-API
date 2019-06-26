5141e32205dd7768728ec941fcc843da20e5e7b0
Roberto Atanásio <roberto.atanasio.pl@gmail.com>
2019-06-26 09:44:18 -0300

    Criando controllers e DTOs

---
+++ src/main/java/com/rapl/ponto/api/utils/StringToLocalDateTIme.java
@@ -0,0 +1,17 @@
+package com.rapl.ponto.api.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StringToLocalDateTIme {
+
+	public static void main(String[] args) {
+
+        String dateTime = "2018-12-11 17:30";
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime formatDateTime = LocalDateTime.parse(dateTime, formatter);
+
+         System.out.println("Parsed Date : " + formatDateTime);
+    }
+}
