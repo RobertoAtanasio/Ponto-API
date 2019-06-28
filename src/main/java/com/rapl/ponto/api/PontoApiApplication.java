3b4029f2b14477fcd99b1e07ed79e46a7dc23511
Roberto Atanásio <roberto.atanasio.pl@gmail.com>
2019-06-27 21:00:54 -0300

    Adicionar ehcache

--- src/main/java/com/rapl/ponto/api/PontoApiApplication.java
+++ src/main/java/com/rapl/ponto/api/PontoApiApplication.java
@@ -2,8 +2,16 @@ package com.rapl.ponto.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+/*
+em PontoApiApplication.java injetar @EnableCaching
+nos méodos de serviço, injetar @Cacheable("lancamentoPorId") e @CachePut("lancamentoPorId")
+O @CachePut deixa em memória para uma posterior pesquisa
+ */
 
 @SpringBootApplication
+@EnableCaching
 public class PontoApiApplication {
 
 	public static void main(String[] args) {
